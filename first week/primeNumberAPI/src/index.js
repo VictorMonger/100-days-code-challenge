c3639f2a40676a076f57fbf18cd17e335d2f535f
Victor Monger <victormonger2@outlook.com>
2024-01-03 16:06:43 -0300

    refactored

--- first week/primeNumberAPI/src/index.js
+++ first week/primeNumberAPI/src/index.js
@@ -1,39 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-app.get('/primeNumber', (request, response) => {
+app.get("/primeNumber", (request, response) => {
   const { number } = request.query;
 
-  console.time('time')
   const numberIsPrime = isPrime(number);
-  console.timeEnd('time')
 
   if (numberIsPrime) {
-    return response.status(200).json({ data: 'Number is prime!'})
+    return response.status(200).json({ data: "Number is prime!" });
   }
-  return response.status(200).json({ data: 'Number is not prime!'})
+  return response.status(200).json({ data: "Number is not prime!" });
 });
 
 const isPrime = (number) => {
-  const halfRange = number / 2;
-  const failsBasicCheck = parseInt(number) === 1 || isDivisibleFor(number, 2) || isDivisibleFor(number, 3) || isDivisibleFor(number, 5);
-  
-  if (failsBasicCheck) {
-    return false
+  if (failsBasicCheck(number)) {
+    return false;
   }
 
-  for (let i = 7; i < halfRange; i++) {
-    if (number % i === 0) {
-      return false
-    };
-  };
-  return true
-}
+  const halfRange = number / 2;
+
+  for (let i = 7; i < halfRange; i = i + 2) {
+    if (isDivisibleFor(number, i)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const failsBasicCheck = (number) => {
+  return (
+    parseInt(number) === 1 ||
+    (isDivisibleFor(number, 2) && parseInt(number) !== 2) ||
+    (isDivisibleFor(number, 3) && parseInt(number) !== 3) ||
+    (isDivisibleFor(number, 5) && parseInt(number) !== 7)
+  );
+};
 
 const isDivisibleFor = (number, divisor) => {
   return number % divisor === 0;
-}
+};
 
 app.listen(3000);
