08fdd7421eb040b79167378fcf568d9ee0bef131
Victor Monger <victormonger2@outlook.com>
2024-01-10 16:54:56 -0300

    tables created

---
+++ second week/gymDataBase.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [IF NOT EXIST] gym (
+  cnpj VARCHAR (100) PRIMARY KEY,
+  name VARCHAR (100) UNIQUE NOT NULL
+);
+
+CREATE TABLE [IF NOT EXIST] students (
+  cpf VARCHAR (11) PRIMARY KEY,
+  name VARCHAR (100), 
+  CONSTRAINT fk_gym
+    FOREIGN KEY (cnpj)
+      REFERENCES gym (cnpj)
+);
+
+CREATE TABLE [IF NOT EXIST] modality (
+  id_modality INT PRIMARY KEY,
+  vacancy_qt INT,
+  description VARCHAR (100) 
+);
+
+CREATE TABLE [IF NOT EXIST] registration (
+  registration_id INT
+  date_registration DATE
+
+);
