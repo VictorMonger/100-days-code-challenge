e0395b6a6b2bba676f04af2d4b7cc412045a9520
Victor Monger <victormonger2@outlook.com>
2024-01-12 17:08:51 -0300

    selects refactored

--- second week/selectGymDataBase.sql
+++ second week/selectGymDataBase.sql
@@ -1,8 +1,17 @@
-SELECT name
-FROM students
-WHERE cnpj = '12345678000100';
+SELECT s.*, g.name gym_name FROM students s
+  INNER JOIN gyms g ON g.cnpj = s.cnpj
+WHERE s.cnpj = '12345678000100';
 
-SELECT cpf
-FROM registrations
-WHERE id = 1;
+SELECT s.*, m.name modality_name FROM students s
+  INNER JOIN registrations r ON r.cpf = s.cpf
+  INNER JOIN modalities m ON m.id = r.modality_id
+WHERE m.id = 1
 
+SELECT s.*, g.name gym_name, m.name modality_name FROM students s
+  INNER JOIN gyms g ON g.cnpj = s.cnpj
+  INNER JOIN registrations r ON r.cpf = s.cpf
+  INNER JOIN modalities m ON m.id = r.modality_id
+WHERE 
+  g.cnpj = '12345678000100' 
+  AND m.id = 1
+  AND registration_date > current_date - interval '1' month
