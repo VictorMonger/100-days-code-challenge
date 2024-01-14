01f48c4af276115abbae99c18a3781c2873d9dd2
Victor Monger <victormonger2@outlook.com>
2024-01-14 11:54:41 -0300

    update with returning

--- second week/gymDatabase/updateGymDatabase.sql
+++ second week/gymDatabase/updateGymDatabase.sql
@@ -1,8 +1,10 @@
 UPDATE gyms
 SET name = 'Changed Gym'
 WHERE cnpj = '23456699000100'
+RETURNING *
 
 UPDATE students
 SET name = 'Diogo Felipe Stefano Monger'
 WHERE cpf = '03098778900'
+RETURNING *
 
