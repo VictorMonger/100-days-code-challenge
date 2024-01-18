cb33830fa46d15d103aa7cc698fedbed29c4e21a
Victor Monger <victormonger2@outlook.com>
2024-01-18 18:53:22 -0300

    controller refactored

--- third week/registrationAPI/src/controllers/userController.js
+++ third week/registrationAPI/src/controllers/userController.js
@@ -1,11 +1,12 @@
-const { modelRegisterUser } = require("../models/userModels")
+const { modelRegisterUser } = require("../models/userModels");
 
 const registerUser = async (request, response) => {
   const { userName, firstName, lastName, email, password } = request.body;
-  const failsInsertCheck = !userName || !firstName || !lastName || !email || !password;
-  
+  const failsInsertCheck =
+    !userName || !firstName || !lastName || !email || !password;
+
   if (failsInsertCheck) {
-    response.status(400).json({ error : "PLEASE ADD ALL FIELDS" });
+    response.status(400).json({ error: "PLEASE ADD ALL FIELDS" });
   }
 
   const user = {
@@ -13,14 +14,14 @@ const registerUser = async (request, response) => {
     firstName,
     lastName,
     email,
-    password
-  }
+    password,
+  };
 
   const userRegistration = await modelRegisterUser(user);
 
   return response.status(201).json(userRegistration);
-}
+};
 
 module.exports = {
   registerUser,
-}
+};
