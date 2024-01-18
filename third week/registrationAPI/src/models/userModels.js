51a4e607b879278088a774482741ac7edeb79c3c
Victor Monger <victormonger2@outlook.com>
2024-01-18 18:54:02 -0300

    model refactored

--- third week/registrationAPI/src/models/userModels.js
+++ third week/registrationAPI/src/models/userModels.js
@@ -1,8 +1,17 @@
 const connection = require("../database/connection");
 
-const modelRegisterUser = () => {
-  
-}
+const modelRegisterUser = async (user) => {
+  const { userName, firstName, lastName, email, password } = user;
+  return await connection("users")
+    .insert({
+      userName,
+      firstName,
+      lastName,
+      email,
+      password,
+    })
+    .returning("*");
+};
 
 module.exports = {
   modelRegisterUser,
