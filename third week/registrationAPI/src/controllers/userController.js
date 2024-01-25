a2bc04ca8178d9c251bcc357e2c201e807ddca41
Victor Monger <victormonger2@outlook.com>
2024-01-25 14:58:55 -0300

    added class to mvc

--- third week/registrationAPI/src/controllers/userController.js
+++ third week/registrationAPI/src/controllers/userController.js
@@ -1,125 +1,122 @@
-const {
-  modelRegisterUser,
-  modelGetAllUsers,
-  modelGetUserById,
-  modelUpdateUser,
-  modelDeleteUser,
-  emailExists,
-  userNameExists,
-  modelUserLogin,
-} = require("../models/userModels");
+class UsersController {
+  constructor(usersModel) {
+    this.usersModel = usersModel;
+  }
+  
+  async register(request, response) {
+    try {
+      const { userName, firstName, lastName, email, password } = request.body;
+      const failsInsert =
+        !userName || !firstName || !lastName || !email || !password;
 
-const registerUser = async (request, response) => {
-  try {
-    const { userName, firstName, lastName, email, password } = request.body;
-    const failsInsert =
-      !userName || !firstName || !lastName || !email || !password;
+      if (failsInsert) {
+        return response
+          .status(400)
+          .json({ error: "Insert all required fields" });
+      }
 
-    if (failsInsert) {
-      return response.status(400).json({ error: "Insert all required fields" });
+      const userNameExist = await this.usersModel.userNameExists(userName);
+
+      if (userNameExist) {
+        return response.status(400).json({ error: "Username already in use" });
+      }
+
+      const userEmailExist = await this.usersModel.emailExists(email);
+
+      if (userEmailExist) {
+        return response
+          .status(400)
+          .json({ error: "E-mail already registered" });
+      }
+
+      const user = {
+        userName,
+        firstName,
+        lastName,
+        email,
+        password,
+      };
+
+      const userRegistration = await this.usersModel.register(user);
+
+      return response.status(201).json(userRegistration);
+    } catch (error) {
+      console.log(error)
+      return response.status(500).json({ error: "Internal Server Error" });
     }
+  }
 
-    const userNameExist = await userNameExists(userName);
+  async login(request, response) {
+    try {
+      const { userName, password } = request.body;
+      const failsInsert = !userName || !password;
 
-    if (userNameExist) {
-      return response.status(400).json({ error: "Username already in use" });
-    };
+      if (failsInsert) {
+        return response
+          .status(400)
+          .json({ error: "Insert all required fields" });
+      }
 
-    const userEmailExist = await emailExists(email);
+      const userLogin = await this.usersModel.login(userName, password);
 
-    if (userEmailExist) {
-      return response.status(400).json({ error: "E-mail already registered" });
+      if (!userLogin) {
+        return response
+          .status(401)
+          .json({ message: "user login or password not valid" });
+      }
+
+      return response.status(200).json({ message: "logged" });
+    } catch (error) {
+      return response.status(500).json({ error: "Internal Server Error" });
     }
-
-    const user = {
-      userName,
-      firstName,
-      lastName,
-      email,
-      password,
-    };
-
-    const userRegistration = await modelRegisterUser(user);
-
-    return response.status(201).json(userRegistration);
-  } catch (error) {
-    return response.status(500).json({ error: "Internal Server Error" });
   }
-};
 
-const userLogin = async (request, response) => {
-  try {
-    const { userName, password } = request.body;
-    const failsInsert = !userName || !password;
+  async getAll(request, response) {
+    try {
+      const users = await this.usersModel.getAll();
 
-    if (failsInsert) {
-      return response.status(400).json({ error: "Insert all required fields" });
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(500).json({ error: "Internal Server Error" });
     }
-
-    const userLogin = await modelUserLogin(userName, password);
-
-    if (!userLogin) {
-      return response.status(401).json({ message: "user login or password not valid"});
-    };
-
-    return response.status(200).json({ message: "logged"});
-  } catch (error) {
-    return response.status(500).json({ error: "Internal Server Error" });
   }
-};
 
-const getAllUsers = async (request, response) => {
-  try {
-    const users = await modelGetAllUsers();
+  async index(request, response) {
+    try {
+      const { id } = request.params;
 
-    return response.status(200).json(users);
-  } catch (error) {
-    return response.status(500).json({ error: "Internal Server Error" });
+      const user = await this.usersModel.getById(id);
+
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(500).json({ error: "Internal Server Error" });
+    }
   }
-};
 
-const getUserById = async (request, response) => {
-  try {
-    const { id } = request.params;
+  async update(request, response) {
+    try {
+      const { id } = request.params;
+      const { userName } = request.body;
 
-    const user = await modelGetUserById(id);
+      const updatedUser = await this.usersModel.update(id, userName);
 
-    return response.status(200).json(user);
-  } catch (error) {
-    return response.status(500).json({ error: "Internal Server Error" });
+      return response.status(200).json(updatedUser);
+    } catch (error) {
+      return response.status(500).json({ error: "Internal Server Error" });
+    }
   }
-};
 
-const updateUser = async (request, response) => {
-  try {
-    const { id } = request.params;
-    const { userName } = request.body;
+  async delete(request, response) {
+    try {
+      const { id } = request.params;
 
-    const updatedUser = await modelUpdateUser(id, userName);
+      const deletedUser = await this.usersModel.delete(id);
 
-    return response.status(200).json(updatedUser);
-  } catch (error) {
-    return response.status(500).json({ error: "Internal Server Error" });
+      return response.status(200).json(deletedUser);
+    } catch (error) {
+      return response.status(500).json({ error: "Internal Server Error" });
+    }
   }
-};
+}
 
-const deleteUser = async (request, response) => {
-  try {
-    const { id } = request.params;
-
-    const deletedUser = await modelDeleteUser(id);
-
-    return response.status(200).json(deletedUser);
-  } catch (error) {
-    return response.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-module.exports = {
-  registerUser,
-  getAllUsers,
-  getUserById,
-  updateUser,
-  deleteUser,
-  userLogin,
-};
+module.exports = UsersController;
--- third week/registrationAPI/src/models/userModels.js
+++ third week/registrationAPI/src/models/userModels.js
@@ -1,102 +1,97 @@
-const connection = require("../database/connection");
-
-const userNameExists = async (userName) => {
-  try {
-    return await connection("users")
-      .select("userName")
-      .where("userName", userName)
-      .first();
-  } catch (error) {
-    throw new Error(error);
+class UsersModel {
+  constructor(connection) {
+    this.connection = connection;
   }
-};
 
-const emailExists = async (email) => {
-  try {
-    return await connection("users")
-      .select("email")
-      .where("email", email)
-      .first();
-  } catch (error) {
-    throw new Error(error);
+  async userNameExists(userName) {
+    try {
+      return await this.connection("users")
+        .select("userName")
+        .where("userName", userName)
+        .first();
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-const modelRegisterUser = async (user) => {
-  try {
-    const { userName, firstName, lastName, email, password } = user;
-
-    return await connection("users")
-      .insert({
-        userName,
-        firstName,
-        lastName,
-        email,
-        password,
-      })
-      .returning("*");
-  } catch (error) {
-    throw new Error(error);
+  async emailExists(email) {
+    try {
+      return await this.connection("users")
+        .select("email")
+        .where("email", email)
+        .first();
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-const modelUserLogin = async (userName, password) => {
-  try {
-    return await connection("users")
-      .select("*")
-      .where("userName", userName)
-      .orWhere("email", userName)
-      .andWhere("password", password)
-      .first();
-  } catch (error) {
-    throw new Error(error);
+  async register(user) {
+    try {
+      const { userName, firstName, lastName, email, password } = user;
+
+      return await this.connection("users")
+        .insert({
+          userName,
+          firstName,
+          lastName,
+          email,
+          password,
+        })
+        .returning("*");
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-const modelGetAllUsers = async () => {
-  try {
-    return await connection("users").select("*");
-  } catch (error) {
-    throw new Error(error);
+  async login(userName, password) {
+    try {
+      return await this.connection("users")
+        .select("*")
+        .where("userName", userName)
+        .orWhere("email", userName)
+        .andWhere("password", password)
+        .first();
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-const modelGetUserById = async (id) => {
-  try {
-    return await connection("users").select("*").where("id", id).first();
-  } catch (error) {
-    throw new Error(error);
+  async getAll() {
+    try {
+      return await this.connection("users").select("*");
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-const modelDeleteUser = async (id) => {
-  try {
-    return await connection("users").where("id", id).delete();
-  } catch (error) {
-    throw new Error(error);
+  async getById(id) {
+    try {
+      return await this.connection("users").select("*").where("id", id).first();
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-const modelUpdateUser = async (id, userName) => {
-  try {
-    return await connection("users").where("id", id).update(
-      {
-        userName,
-      },
-      ["id", "userName"]
-    );
-  } catch (error) {
-    throw new Error(error);
+  async delete(id) {
+    try {
+      return await this.connection("users").where("id", id).delete();
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-module.exports = {
-  modelRegisterUser,
-  modelGetAllUsers,
-  modelGetUserById,
-  modelUpdateUser,
-  modelDeleteUser,
-  emailExists,
-  userNameExists,
-  modelUserLogin,
-};
+  async update(id, userName) {
+    try {
+      return await this.connection("users").where("id", id).update(
+        {
+          userName,
+        },
+        ["id", "userName"]
+      );
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
+
+module.exports = UsersModel;
