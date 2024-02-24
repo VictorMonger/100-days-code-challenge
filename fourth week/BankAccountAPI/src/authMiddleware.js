41bb4c9915f1f7459965f2bd80b119a9ae265d43
Victor Monger <victormonger2@outlook.com>
2024-02-24 13:53:25 -0300

    auth middleware added

---
+++ fourth week/BankAccountAPI/src/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const { JWT_PRIVATE_KEY } = process.env;
+
+const verifyToken = (request, response, next) => {
+  try {
+    const { authorization } = request.headers;
+    const [, token] = authorization.split(" ");
+
+    if (!token) {
+      return response.status(401).json({ error: "Access denied" });
+    }
+
+    const payload = jwt.verify(token, JWT_PRIVATE_KEY);
+    request.headers["client"] = payload.client;
+
+    next();
+  } catch (error) {
+    response.status(401).json({ error: "Invalid token" });
+  }
+};
+
+module.exports = { verifyToken };
