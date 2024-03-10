5b3b758fdfa96d91a928c3e1457628ef0daa018d
Victor Monger <victormonger2@outlook.com>
2024-03-09 23:46:10 -0300

    bank account controller init

--- fourth week/BankAccountAPI/src/controllers/bankAccountController.js
+++ fourth week/BankAccountAPI/src/controllers/bankAccountController.js
@@ -2,6 +2,16 @@ class BankAccountController {
   constructor(bankAccountModel) {
     this.bankAccountModel = bankAccountModel;
   }
+  
+  async createAccount(request,response) {
+    try {
+      const { balance, cpf } = request.body
+      
+    } catch (error) {
+      
+    }
+  }
+
 }
 
 module.exports = BankAccountController;
