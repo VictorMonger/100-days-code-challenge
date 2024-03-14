94f7beeede90307ea2b76c511a8ccd1a35222ec9
Victor Monger <victormonger2@outlook.com>
2024-03-14 20:17:47 -0300

    create account added on controllers

--- fourth week/BankAccountAPI/src/controllers/bankAccountController.js
+++ fourth week/BankAccountAPI/src/controllers/bankAccountController.js
@@ -2,16 +2,33 @@ class BankAccountController {
   constructor(bankAccountModel) {
     this.bankAccountModel = bankAccountModel;
   }
-  
-  async createAccount(request,response) {
+
+  async createAccount(request, response) {
     try {
-      const { balance, cpf } = request.body
-      
+      const { cpf, balance } = request.body;
+      const failsInsert = !cpf || !balance;
+
+      if (failsInsert) {
+        return response
+          .status(400)
+          .json({ error: "Insert all required fields" });
+      }
+
+      const createAccount = await this.bankAccountModel.createAccount(cpf, balance);
+
+      return response.status(201).json(createAccount);
     } catch (error) {
-      
+      return response.status(500).json({ error: "Internal Server Error" });
     }
   }
 
+  async deposit(request, response) {
+
+  }
+
+  async withdrawal(request, response) {
+    
+  }
 }
 
 module.exports = BankAccountController;
