1e7c24035f9a4f5421bef4b9e7e60bc4092cdc8e
Victor Monger <victormonger2@outlook.com>
2024-03-14 20:18:06 -0300

    create account added on models

--- fourth week/BankAccountAPI/src/models/bankAccountModel.js
+++ fourth week/BankAccountAPI/src/models/bankAccountModel.js
@@ -3,8 +3,27 @@ class BankAccountModel {
     this.connection = connection;
   }
 
-  async createAccount(cpf) {
+  async createAccount(cpf, balance) {
+    try {
+      const { cpf, balance} = client;
 
+      return await this.connection("accounts")
+        .insert({
+          cpf,
+          balance,
+        })
+        .returning("*");
+  } catch (error) {
+    throw new Error(error);
+  }
+  }
+
+  async deposit() {
+
+  }
+
+  async withdrawal() {
+    
   }
 }
 
